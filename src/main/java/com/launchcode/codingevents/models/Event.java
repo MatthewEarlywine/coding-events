3547ff4e26f815be6d05c56f585a616d0fad905a
Matt <matthewearlywine@gmail.com>
2021-06-25 23:12:08 -0500

    Commit for Ch.18 - Relationships in Object-Relational Mapping - 18.4 - One-to-One Relationships

--- src/main/java/com/launchcode/codingevents/models/Event.java
+++ src/main/java/com/launchcode/codingevents/models/Event.java
@@ -1,11 +1,8 @@
 package com.launchcode.codingevents.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
-import java.util.Objects;
 
 @Entity
 public class Event extends AbstractEntity {
@@ -14,31 +11,17 @@ public class Event extends AbstractEntity {
     @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters.")
     private String name;
 
-    @Size(max = 500, message = "Description too long!")
-    private String description;
-
-    @NotBlank(message = "Email is required.")
-    @Email(message = "Invalid Email. Try Again.")
-    private String contactEmail;
-
-    @NotBlank(message = "Address is required.")
-    @NotNull(message = "Address must be visible.")
-    private String address;
-
-    @Min(value = 1, message = "Someone must attend the event!")
-    private int attendees;
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
 
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
 
-    public Event(String name, String description, String contactEmail,
-                 String address, int attendees, EventCategory eventCategory) {
+    public Event(String name, EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
-        this.contactEmail = contactEmail;
-        this.address = address;
-        this.attendees = attendees;
         this.eventCategory = eventCategory;
     }
 
@@ -52,44 +35,20 @@ public class Event extends AbstractEntity {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getAttendees() {
-        return attendees;
-    }
-
-    public void setAttendees(int attendees) {
-        this.attendees = attendees;
-    }
-
     public EventCategory getEventCategory() {
         return eventCategory;
     }
 
     public void setEventCategory(EventCategory eventCategory) {
         this.eventCategory = eventCategory;
+    }
+
+    public EventDetails getEventDetails() {
+        return eventDetails;
+    }
+
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
     }
 
     @Override
---
+++ src/main/java/com/launchcode/codingevents/models/EventDetails.java
@@ -0,0 +1,67 @@
+package com.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.*;
+
+@Entity
+public class EventDetails extends AbstractEntity{
+
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid Email. Try Again.")
+    private String contactEmail;
+
+    @NotBlank(message = "Address is required.")
+    @NotNull(message = "Address must be visible.")
+    private String address;
+
+    @Min(value = 1, message = "Someone must attend the event!")
+    private int attendees;
+
+    public EventDetails(@Size(max = 500, message = "Description too long!") String description,
+                        @NotBlank(message = "Email is required.") @Email(message = "Invalid Email. Try Again.")String contactEmail,
+                        @NotBlank(message = "Address is required.")  @NotNull(message = "Address must be visible.") String address,
+                        @Min(value = 1, message = "Someone must attend the event!") int attendees) {
+        this.description = description;
+        this.contactEmail = contactEmail;
+        this.address = address;
+        this.attendees = attendees;
+    }
+
+    public EventDetails (){}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(int attendees) {
+        this.attendees = attendees;
+    }
+}
